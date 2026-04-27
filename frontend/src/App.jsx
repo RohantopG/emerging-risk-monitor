3c0759918634f2cffaaa202b6c62e4cb3dd39b79
Rajeshwari MB <rajeshwarirm2004@gmail.com>
2026-04-27 20:34:10 +0530

    Day 6: Dashboard with KPI cards and bar chart using dummy data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,7 @@
-import { useContext } from "react";
-import Login from "./pages/Login";
-import ListPage from "./pages/ListPage";
-import { AuthProvider, AuthContext } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-function AppContent() {
-  const { isAuthenticated, login } = useContext(AuthContext);
-
-  return isAuthenticated ? (
-    <ProtectedRoute>
-      <ListPage />
-    </ProtectedRoute>
-  ) : (
-    <Login onLogin={login} />
-  );
-}
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return (
-    <AuthProvider>
-      <AppContent />
-    </AuthProvider>
-  );
+  return <Dashboard />;
 }
 
 export default App;
